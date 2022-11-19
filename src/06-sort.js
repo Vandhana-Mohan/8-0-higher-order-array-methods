b8862ee09a1589186e7a805b8e912c4e0b824e1f
Vandhana-Mohan <mohan.vandhana@gmail.com>
2022-11-19 02:16:43 -0500

    Finish sort with comments

--- src/06-sort.js
+++ src/06-sort.js
@@ -4,18 +4,23 @@
   You may use this data to test your functions. You may assume the shape of the data remains the same but that the values may change.
 
   Keep in mind that your functions must still have and use a parameter for accepting all songs.
-*/
-const exampleSongData = require("../data/songs");
-// Do not change the line above.
+  */
+  const exampleSongData = require("../data/songs");
+  // Do not change the line above.
 
-/**
+  /**
  * Reorders the array so that the song objects are organized by their runtime. The shortest song should come first.
  * @param {Object[]} songs - An array of songs. See the song data for more.
  * @returns {Object[]}
  */
-function sortByRuntimeAscending(songs) {}
 
-/**
+
+  // -------------  sorts array ascending - one liner - uses sort & Arrow function -----------------
+  // a,b => a - b == sorts in ascending order
+  const sortByRuntimeAscending = (songs) => songs.sort((a,b) => a.runtimeInSeconds - b. runtimeInSeconds)
+
+
+  /**
  * Reorders the array so that the song objects are organized by their artist name. The artist that comes last in the alphabet should come first.
  *
  * TIP: "A" and "a" sort differently. To avoid this issue, you may want to use `.toLowerCase()` or `.toUpperCase()`.
@@ -23,7 +28,14 @@ function sortByRuntimeAscending(songs) {}
  * @param {Object[]} songs - An array of songs. See the song data for more.
  * @returns {Object[]}
  */
-function sortByArtistNameDescending(songs) {}
+
+
+ // -------------  sorts artist name in descending - one liner - uses sort & Arrow function -----------------
+ // b.toLowerCase() - a.toLowerCase() ? 1 : -1 == converts all string to lowercase and then compares. If value is greater, then 1, that means its sorted in descending order
+ // we can also use .LocaleCompare() which i have used for next question
+
+ const sortByArtistNameDescending = ((songs) => songs.sort((a, b) => b.artist.toLowerCase() > a.artist.toLowerCase() ? 1 : -1))
+
 
 /**
  * Reorders the array so that the song objects are organized by their song title. The title that comes first in the alphabet should come first.
@@ -33,7 +45,14 @@ function sortByArtistNameDescending(songs) {}
  * @param {Object[]} songs - An array of songs. See the song data for more.
  * @returns {Object[]}
  */
-function sortBySongTitleAscending(songs) {}
+
+
+
+ // -------------  sorts song title in ascending - one liner - uses sort & Arrow function -----------------
+ // Here we use .LocaleCompare() which compares ASCII value before sorting.
+ // Used an alternative for previous function to show variation - both method works
+
+ const sortBySongTitleAscending = (songs) => songs.sort((a, b) => a.title.localeCompare(b.title))
 
 module.exports = {
   sortByRuntimeAscending,
